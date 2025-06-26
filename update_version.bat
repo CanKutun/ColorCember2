630d4e7fc94bb0e63845aad9bcf60e76da016c12
Can Kutun <cankutun@yahoo.com>
2025-06-27 00:47:05 +0300

    Versiyon 2.3.1 olarak guncellendi

--- update_version.bat
+++ update_version.bat
@@ -1,9 +1,16 @@
+
 @echo off
 
 REM GitHub projesinin yerel klasör yolunu buraya yaz
-cd /d C:\Users\canku\COLREKODULLUYEDEK
+cd C:\Users\canku\COLREKODULLUYEDEK
 
-REM version.txt dosyasına en son sürümü yaz (buraya yeni sürümü yazabilirsin)
+REM Main branch'e geç (detached HEAD durumunu düzeltir)
+git checkout main
+
+REM Uzak depo ile yeniden senkronize ol
+git pull --rebase origin main
+
+REM version.txt dosyasına en son sürümü yaz
 echo 2.3.1 > version.txt
 
 REM Değişiklikleri git’e ekle
